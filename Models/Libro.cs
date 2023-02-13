ffa1eab9a91ce560f1630201ea9da20ed6000f4e
Jorge Toribio <jorgeluistoribio14@gmail.com>
2023-02-13 18:20:01 -0400

    Libro class

---
+++ Models/Libro.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Libro
+{
+    [Key]
+    public int LibroId { get; set; }
+
+    [Required(ErrorMessage = "Error, favor intente de nuevo")]
+    public string Titulo { get; set; }
+    public double Precio { get; set; }
+}
